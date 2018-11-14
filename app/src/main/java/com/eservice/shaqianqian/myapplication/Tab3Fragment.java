1de5db2e87dd1ececeba4e5bd2d994a8aee559a1
shaqianqian <sqq940814@gmail.com>
2018-11-14 12:42:35 +0100

    remove ignore

---
+++ app/src/main/java/com/eservice/shaqianqian/myapplication/Tab3Fragment.java
@@ -0,0 +1,24 @@
+package com.eservice.shaqianqian.myapplication;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Tab3Fragment extends Fragment {
+
+    //ici on cree un fragement pour le tab<Tab3Fragement>
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view;
+        view = inflater.inflate(R.layout.tab_layout, container, false);
+        TextView ville = (TextView) view.findViewById(R.id.textView);
+        ville.setText("Lyon");
+        return view;
+    }
+
+
+}
